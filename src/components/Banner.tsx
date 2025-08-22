95c6c96e54d3271025fe07d315a6949cde1117dc
Kafi <ahkafi05@gmail.com>
2025-08-23 01:34:05 +0600

    banner overflow fixed

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -7,6 +7,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import Link from 'next/link';
 
 const Banner = () => {
     return (
@@ -15,7 +16,7 @@ const Banner = () => {
                     spaceBetween={30}
                     centeredSlides={true}
                     autoplay={{
-                    delay: 2500,
+                    delay: 3000,
                     disableOnInteraction: false,
                     }}
                     pagination={{
@@ -26,21 +27,23 @@ const Banner = () => {
                     className="mySwiper lg:h-100 h-max rounded-xl"
                 >
                 <SwiperSlide> 
-                    <div className='bg-[#d9f1f4] h-100 rounded-sm flex justify-between px-20 mt-5'>
+                    <div className='bg-[#d9f1f4] rounded-xl flex flex-col lg:flex-row justify-between lg:px-20 mt-5'>
                         {/* text */}
-                        <div className='flex flex-col justify-center'>
-                            <h2 className='font-bold text-5xl text-gray-900'>Save Up to 50% on Selected Subscription</h2>
-                            <Button className='self-start mt-5'>Order Now</Button>
+                        <div className='flex flex-col justify-center lg:w-sm p-4'>
+                            <h2 className='font-bold text-xl lg:text-5xl text-gray-900'>Save Up to 50% on Selected Subscription</h2>
+                            <Button className='self-start mt-2 lg:mt-5' asChild>
+                                <Link href='/products'>Order Now</Link></Button>
                         </div>
                         <Image src={GirlWIthHeadphone} alt='girl with headphone'></Image>
                     </div>
             </SwiperSlide>
                             <SwiperSlide> 
-                    <div className='bg-[#d9f1f4] h-100 rounded-sm flex justify-between px-20 mt-5'>
+                    <div className='bg-[#d9f1f4] lg:h-100 rounded-xl flex flex-col lg:flex-row justify-between lg:px-20 mt-5'>
                         {/* text */}
-                        <div className='flex flex-col justify-center'>
-                            <h2 className='font-bold text-5xl text-gray-900'>Save Up to 50% on Selected Subscription</h2>
-                            <Button className='self-start mt-5'>Order Now</Button>
+                        <div className='flex flex-col justify-center lg:w-sm p-4'>
+                            <h2 className='font-bold text-xl lg:text-5xl text-gray-900'>Save Up to 50% on Selected Subscription</h2>
+                            <Button className='self-start mt-2 lg:mt-5' asChild>
+                                <Link href='/products'>Order Now</Link></Button>
                         </div>
                         <Image src={GirlWIthHeadphone} alt='girl with headphone'></Image>
                     </div>
